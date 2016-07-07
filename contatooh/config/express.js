3b009b8aec647851f5793b3ddac11b1cad07b99b
Devair Luiz Moitim <dmoitim@gmail.com>
2016-07-07 13:28:37 -0300

    Inclusão e configuração da rota e do controller home para devolver a página inicial.

--- contatooh/config/express.js
+++ contatooh/config/express.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var home = require('../app/routes/home');
 
 module.exports = function(){
     var app = express();
@@ -8,6 +9,13 @@ module.exports = function(){
 
     //Middleware
     app.use(express.static('./public'));
+
+    //Configuração do template engine EJS
+    app.set('view engine', 'ejs');
+    app.set('views', './app/views');
+
+    //Rotas
+    home(app);
 
     return app;
 }
--- contatooh/index.js
+++ contatooh/index.js
@@ -3,4 +3,4 @@ var app = require('./config/express')();
 
 http.createServer(app).listen(app.get('port'), function(){
     console.log('Express Server escutando na porta ' + app.get('port'));
-})
+});
