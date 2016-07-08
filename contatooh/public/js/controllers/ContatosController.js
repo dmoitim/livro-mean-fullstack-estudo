836e61a942b15a58d838fcc350f9cf528b56e3fe
Devair Luiz Moitim <dmoitim@gmail.com>
2016-07-08 16:33:13 -0300

    Alteração para $resource no consumo da API express e inclusão da remoção de contatos.

--- contatooh/public/js/controllers/ContatosController.js
+++ contatooh/public/js/controllers/ContatosController.js
@@ -1,19 +1,29 @@
-angular.module('contatooh').controller('ContatosController', function($scope, $http){
+angular.module('contatooh').controller('ContatosController', function($scope, $resource){
     $scope.contatos = [];
     $scope.filtro = '';
 
-    var promisse = $http({
-        method: 'GET',
-        url: '/contatos'
-    });
+    var Contato = $resource('/contatos/:id');
 
-    promisse
-        .success(function(data, status, headers, config){
-            $scope.contatos = data;
-        })
-        .error(function(statusText, status, headers, config){
-            console.log('Não foi possível obter a lista de contatos.');
-            console.log(statusText);
-            console.log(status);
-        });
+    function buscaContatos(){
+        Contato.query(
+            function(contatos){
+                $scope.contatos = contatos;
+            },
+            function(erro) {
+                console.log(erro);
+                console.log('Não foi possível obter a lista de contatos.');
+            }
+        );
+    }
+    buscaContatos();
+
+    $scope.remove = function(contato){
+        Contato.delete({id: contato._id},
+            buscaContatos,
+            function(erro){
+                console.log(erro);
+                console.log('Não foi possível remover o contato.');
+            }
+        );
+    }
 });
