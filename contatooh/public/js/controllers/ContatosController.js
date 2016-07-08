a120345a6a45c7106b5ef0a9f608ee2fb85d288b
Devair Luiz Moitim <dmoitim@gmail.com>
2016-07-08 15:43:54 -0300

    Ligação do front-end com o back-end através de $http

--- contatooh/public/js/controllers/ContatosController.js
+++ contatooh/public/js/controllers/ContatosController.js
@@ -1,15 +1,19 @@
-angular.module('contatooh').controller('ContatosController', function($scope){
-    $scope.total = 0;
-
-    $scope.incrementa = function() {
-        $scope.total++;
-    }
-
-    $scope.contatos = [
-        {'_id': 1, nome: 'Contato Angular 1', 'email': 'cont1@empresa.com.br'},
-        {'_id': 2, nome: 'Contato Angular 2', 'email': 'cont2@empresa.com.br'},
-        {'_id': 3, nome: 'Contato Angular 3', 'email': 'cont3@empresa.com.br'}
-    ];
-
+angular.module('contatooh').controller('ContatosController', function($scope, $http){
+    $scope.contatos = [];
     $scope.filtro = '';
+
+    var promisse = $http({
+        method: 'GET',
+        url: '/contatos'
+    });
+
+    promisse
+        .success(function(data, status, headers, config){
+            $scope.contatos = data;
+        })
+        .error(function(statusText, status, headers, config){
+            console.log('Não foi possível obter a lista de contatos.');
+            console.log(statusText);
+            console.log(status);
+        });
 });
